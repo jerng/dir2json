583e8d0213faa822709b13288ce52cc2f6ed99ab
Jerng <yangjerng@gmail.com>
2019-08-01 08:48:09 +0800

    Added a primitive progress counter.

--- put-this-in-a-zeppelin-notebook.py
+++ put-this-in-a-zeppelin-notebook.py
@@ -1,14 +1,14 @@
+import datetime
+import json
 import subprocess
+import sys
+import uuid
 
 output = subprocess.check_output(['pip','install', 'tika'])
 print(output)
 
 import tika
 from tika import parser
-
-import datetime
-import json
-import uuid
 
 gen = os.walk('/hostdata')
 
@@ -17,7 +17,9 @@ file_name_list = []
 for dirpath, dirnames, filenames in gen:
     for filename in filenames:
         
-        # DEV: START: limit to five files only
+        # DEV: START LIMIT: limit to five files only
+            # Unbound Test (data, machine): 15191 files FINISHED in 6 min 48 sec.
+            # EXAMINIE: Tika server returns a number of 415 and 422 errors.
         if file_count == 5:
             break
         
@@ -26,17 +28,24 @@ for dirpath, dirnames, filenames in gen:
         # filename = '2005-05-11 photo baxter IMG_0002.JPG'
         
         filestat = os.stat(dirpath + '/'  + filename)
+        parsed = parser.from_file(dirpath + '/' + filename)
+        
         file_name_list.append({ str(uuid.uuid4()) : {   'dir_path' : dirpath, 
                                                         'file_name': filename,
                                                         'byte_size': filestat.st_size,
                                                         'unix_ctime': str(datetime.datetime.utcfromtimestamp(filestat.st_ctime)),
                                                         'unix_mtime': str(datetime.datetime.utcfromtimestamp(filestat.st_mtime)),
                                                         'unix_atime': str(datetime.datetime.utcfromtimestamp(filestat.st_atime)),
-                                                        'tika_metadata:' : parser.from_file(dirpath + '/' + filename)['metadata'],
+                                                        'tika_metadata:' : (parsed['metadata'] if ('metadata' in parsed) else {}),
                                                         }})
+        # BLOCK: print a counter.
+        sys.stdout.write('\r')
+        sys.stdout.write(str(file_count))
+        sys.stdout.flush()
         
-        # DEV: END: limit to five files only
+        # DEV: END LIMIT: limit to five files only
         file_count += 1
-        
-json_data = json.dumps(file_name_list[0:5], indent=4)
-print(json_data)
+
+# BLOCK: Print dictionary as JSON.        
+#json_data = json.dumps(file_name_list[0:5], indent=4)
+#print(json_data)
